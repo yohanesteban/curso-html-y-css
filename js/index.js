76f2867eb25985e811a95fc85eae2b1d8172196e
yohanesteban <emunozmontero04@gmail.com>
2021-08-30 17:56:45 -0500

    76-82

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log('hola mundo')
+var string;('hola gente de yutu');
+let MiPrimeraVariable = 'mi primera variable'
+console.log(MiPrimeraVariable);
+
+
+MiPrimeraVariable = 'esto ha cambiado'
+console.log(MiPrimeraVariable);
