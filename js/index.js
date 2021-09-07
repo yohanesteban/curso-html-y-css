1e7f9b1488b21998cd0a3c4f1a0c3fd842a69485
yohanesteban <emunozmontero04@gmail.com>
2021-09-07 18:20:55 -0500

    84-86

--- js/index.js
+++ js/index.js
@@ -10,20 +10,29 @@ console.log(MiPrimeraVariable);
 let miboolean = true
 let miOtroBoolean = false
 
-let miboolean = true
-let miOtroBoolean = false
-
 let miNumero1 = 0 
 let miNumero2 = 12 
 let miNumero3 = 21
 
 
 let undef
-conosle.log(undef);
+console.log(undef);
 
 let nulo = null
 
-conosle.log(nulo)
+console.log(nulo);
+
+const miprimerObjeto = {
+    unNumero: 12,
+    unstring: 'Esta cadena de caracteres',
+    unacondicion: true,
+};
+
+
+console.log(miObjeto);
+
+
+
 
 
 
