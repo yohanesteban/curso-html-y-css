645db75cc02ec8be040bd3be1ac4f5d20b1bbe52
yohanesteban <emunozmontero04@gmail.com>
2021-09-06 21:35:16 -0500

    82-86

--- js/index.js
+++ js/index.js
@@ -6,3 +6,25 @@ console.log(MiPrimeraVariable);
 
 MiPrimeraVariable = 'esto ha cambiado'
 console.log(MiPrimeraVariable);
+
+let miboolean = true
+let miOtroBoolean = false
+
+let miboolean = true
+let miOtroBoolean = false
+
+let miNumero1 = 0 
+let miNumero2 = 12 
+let miNumero3 = 21
+
+
+let undef
+conosle.log(undef);
+
+let nulo = null
+
+conosle.log(nulo)
+
+
+
+
