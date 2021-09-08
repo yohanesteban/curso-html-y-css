80bd667a52cfc5f747919c6ebe5d0d09aa29fc33
yohanesteban <emunozmontero04@gmail.com>
2021-09-08 18:49:03 -0500

    86-89

--- js/index.js
+++ js/index.js
@@ -29,8 +29,44 @@ const miprimerObjeto = {
 };
 
 
-console.log(miObjeto);
+console.log(miprimerObjeto);
 
+
+const suma = 1 + 2 
+const restar = 1 - 4
+const multiplicacion = 2 * 5
+const division = 5 / 3 
+console.log(suma,restar,multiplicacion,division)
+
+const modulo = 10 % 3
+console.log(modulo)
+
+const num = 5 
+console.log(modulo)
+
+num+
+num+
+num+
+num+
+num+
+console.log(num)
+
+
+const resultado1 = 5 === 6
+const resultado2 = 5 === '5'
+
+const resultado3 = 5<6
+const resultado4 = 5<5
+const resultado5 = 5<5
+const resultado6 = 5<4
+const resultado7 = 5<3
+const resultado8 = 5<34
+const resultado9 = 5<2
+const resultado10 = 5<6
+
+const resultado11 = 5<1
+const resultado12 = 5<63
+console.log(resultado3,resultado4)
 
 
 
